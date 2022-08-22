fce92fcc38526e036da32f75058704963a248be5
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-08-22 15:50:40 +0900

    refactor: Test helpers and repo structure

--- test/foundry/utils/TestHelpers.sol
+++ test/foundry/utils/TestHelpers.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Test} from "../../lib/forge-std/src/Test.sol";
+import {Test} from "../../../lib/forge-std/src/Test.sol";
 
 abstract contract TestHelpers is Test {
     modifier asPrankedUser(address user) {

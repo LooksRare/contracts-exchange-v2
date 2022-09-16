4b5d02926bd329357fc8effc3c556cba5d82ac11
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-09-16 10:11:01 +0900

    feat: Event for ProtocolFee payments

--- contracts/interfaces/ILooksRareProtocol.sol
+++ contracts/interfaces/ILooksRareProtocol.sol
@@ -14,6 +14,9 @@ interface ILooksRareProtocol {
     // Events
     event NewDomainSeparator();
 
+    event ProtocolPayment(address currency, uint256 protocolFee);
+    event ProtocolPaymentWithReferrer(address currency, uint256 protocolFee, address referrer, uint256 referralFee);
+
     event TakerBid(
         uint128 orderNonce,
         address bidUser,

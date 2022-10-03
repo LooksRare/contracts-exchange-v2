412b7200a50f4f88893aa7a247acd09fbe9802a7
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-10-03 13:01:01 +0900

    feat: Helper for slippage parameter

--- contracts/LooksRareProtocolHelpers.sol
+++ contracts/LooksRareProtocolHelpers.sol
@@ -3,12 +3,15 @@ pragma solidity ^0.8.14;
 
 // LooksRare unopinionated libraries
 import {SignatureChecker} from "@looksrare/contracts-libs/contracts/SignatureChecker.sol";
+import {IERC2981} from "@looksrare/contracts-libs/contracts/interfaces/generic/IERC2981.sol";
 
 // Order structs
 import {OrderStructs} from "./libraries/OrderStructs.sol";
 
 // Interfaces
+import {IFeeManager} from "./interfaces/IFeeManager.sol";
 import {IRoyaltyFeeRegistry} from "./interfaces/IRoyaltyFeeRegistry.sol";
+import {IStrategyManager} from "./interfaces/IStrategyManager.sol";
 
 // LooksRareProtocol
 import {LooksRareProtocol} from "./LooksRareProtocol.sol";
@@ -63,14 +66,23 @@ contract LooksRareProtocolHelpers is SignatureChecker {
         public
         view
         returns (
-            uint256 protocolFee,
+            uint256 protocolFeeAmount,
             uint16 collectionDiscountFactor,
-            uint256 royaltyFee,
+            uint256 royaltyFeeAmount,
             address royaltyRecipient,
+            uint256 netPrice,
             RoyaltyType royaltyType
         )
     {
-        //
+        IStrategyManager.Strategy memory strategyInfo = looksRareProtocol.strategyInfo(strategyId);
+
+        (royaltyRecipient, royaltyFeeAmount, royaltyType) = strategyInfo.hasRoyalties
+            ? _getRoyaltyRecipientAndAmountAndRoyaltyType(collection, itemIds, price)
+            : (address(0), 0, RoyaltyType.NoRoyalty);
+
+        protocolFeeAmount = ((price * strategyInfo.protocolFee) / 10000);
+        collectionDiscountFactor = looksRareProtocol.collectionDiscountFactor(collection);
+        netPrice = price - protocolFeeAmount - royaltyFeeAmount;
     }
 
     /**
@@ -148,5 +160,50 @@ contract LooksRareProtocolHelpers is SignatureChecker {
     function computeDigestMerkleRoot(OrderStructs.MerkleRoot memory merkleRoot) public view returns (bytes32 digest) {
         (, , bytes32 domainSeparator, ) = looksRareProtocol.information();
         return keccak256(abi.encodePacked(_ENCODING_PREFIX, domainSeparator, merkleRoot.hash()));
+    }
+
+    function _getRoyaltyRecipientAndAmountAndRoyaltyType(
+        address collection,
+        uint256[] memory itemIds,
+        uint256 amount
+    )
+        internal
+        view
+        returns (
+            address royaltyRecipient,
+            uint256 royaltyAmount,
+            RoyaltyType royaltyType
+        )
+    {
+        // 1. Royalty fee registry
+        (royaltyRecipient, royaltyAmount) = royaltyFeeRegistry.royaltyInfo(collection, amount);
+
+        if (royaltyRecipient != address(0) || royaltyAmount != 0) {
+            // Although royalties would not get paid at the moment using the registry, it can change in the future.
+            royaltyType = RoyaltyType.RoyaltyFeeRegistry;
+        }
+
+        // 2. ERC2981 logic
+        if (royaltyRecipient == address(0) && royaltyAmount == 0) {
+            (bool status, bytes memory data) = collection.staticcall(
+                abi.encodeWithSelector(IERC2981.royaltyInfo.selector, itemIds[0], amount)
+            );
+
+            if (status) {
+                (royaltyRecipient, royaltyAmount) = abi.decode(data, (address, uint256));
+                // The response is valid so royaltyType becomes EIP2981
+                royaltyType = RoyaltyType.RoyaltyEIP2981;
+            }
+
+            if (status && itemIds.length > 1) {
+                for (uint256 i = 1; i < itemIds.length; i++) {
+                    (address royaltyRecipientForToken, uint256 royaltyAmountForToken) = IERC2981(collection)
+                        .royaltyInfo(itemIds[i], amount);
+
+                    if (royaltyRecipientForToken != royaltyRecipient || royaltyAmount != royaltyAmountForToken)
+                        revert IFeeManager.BundleEIP2981NotAllowed(collection, itemIds);
+                }
+            }
+        }
     }
 }

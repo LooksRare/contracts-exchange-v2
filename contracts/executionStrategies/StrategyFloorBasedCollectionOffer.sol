7cf69827b447d099eb2f393ebd46293f15c84d14
hiroshi <hiroshi@looksrare.org>
2022-10-18 14:16:04 +0100

    feat: Floor based collection offer

---
+++ contracts/executionStrategies/StrategyFloorBasedCollectionOffer.sol
@@ -0,0 +1,135 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {OwnableTwoSteps} from "@looksrare/contracts-libs/contracts/OwnableTwoSteps.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {IExecutionStrategy} from "../interfaces/IExecutionStrategy.sol";
+import {OrderStructs} from "../libraries/OrderStructs.sol";
+
+/**
+ * @title StrategyFloorBasedCollectionOffer
+ * @notice This contract allows a bidder to place a discounted floor price bid
+ * @author LooksRare protocol team (👀,💎)
+ */
+contract StrategyFloorBasedCollectionOffer is IExecutionStrategy, OwnableTwoSteps {
+    // Address of the protocol
+    address public immutable LOOKSRARE_PROTOCOL;
+    uint256 public maximumLatency;
+    mapping(address => address) public priceFeeds;
+
+    error InvalidChainlinkPrice();
+    error LatencyToleranceTooHigh();
+    error PriceNotRecentEnough();
+    error PriceFeedNotAvailable();
+
+    /**
+     * @notice Emitted when the maximum Chainlink price latency is updated
+     * @param maximumLatency Maximum Chainlink price latency
+     */
+    event MaximumLatencyUpdated(uint256 maximumLatency);
+
+    /**
+     * @notice Emitted when a collection's price feed address is updated
+     * @param collection NFT collection address
+     * @param priceFeed Chainlink price feed address
+     */
+    event PriceFeedUpdated(address indexed collection, address indexed priceFeed);
+
+    /**
+     * @notice Constructor
+     * @param _looksRareProtocol Address of the LooksRare protocol
+     */
+    constructor(address _looksRareProtocol) {
+        LOOKSRARE_PROTOCOL = _looksRareProtocol;
+    }
+
+    /**
+     * @inheritdoc IExecutionStrategy
+     */
+    function executeStrategyWithTakerBid(OrderStructs.TakerBid calldata, OrderStructs.MakerAsk calldata)
+        external
+        pure
+        override
+        returns (
+            uint256,
+            uint256[] memory,
+            uint256[] memory,
+            bool
+        )
+    {
+        revert OrderInvalid();
+    }
+
+    /**
+     * @inheritdoc IExecutionStrategy
+     */
+    function executeStrategyWithTakerAsk(
+        OrderStructs.TakerAsk calldata takerAsk,
+        OrderStructs.MakerBid calldata makerBid
+    )
+        external
+        view
+        override
+        returns (
+            uint256 price,
+            uint256[] memory itemIds,
+            uint256[] memory amounts,
+            bool isNonceInvalidated
+        )
+    {
+        if (msg.sender != LOOKSRARE_PROTOCOL) revert WrongCaller();
+
+        if (takerAsk.itemIds.length != 1 || takerAsk.amounts.length != 1 || takerAsk.amounts[0] != 1)
+            revert OrderInvalid();
+
+        address priceFeedAddress = priceFeeds[makerBid.collection];
+        if (priceFeedAddress == address(0)) revert PriceFeedNotAvailable();
+
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(priceFeeds[makerBid.collection]);
+        (, int256 answer, , uint256 updatedAt, ) = priceFeed.latestRoundData();
+        if (answer < 0) revert InvalidChainlinkPrice();
+        if (block.timestamp - updatedAt > maximumLatency) revert PriceNotRecentEnough();
+
+        uint256 discountAmount = abi.decode(makerBid.additionalParameters, (uint256));
+        uint256 floorPrice = uint256(answer);
+        uint256 desiredPrice = floorPrice - discountAmount;
+
+        if (desiredPrice > makerBid.maxPrice) {
+            price = makerBid.maxPrice;
+        } else {
+            price = desiredPrice;
+        }
+
+        if (price < takerAsk.minPrice) revert BidTooLow();
+
+        itemIds = takerAsk.itemIds;
+        amounts = takerAsk.amounts;
+        isNonceInvalidated = true;
+    }
+
+    // TODO: Not sure if the 3,600 seconds update frequency also holds true for
+    // NFT floor price oracles, we will have to confirm with Chainlink when we
+    // approach them for mainnet access.
+    /**
+     * @notice Set maximum Chainlink price latency. It cannot be higher than 3,600
+     *         as Chainlink will at least update the price every 3,600 seconds.
+     * @dev Function only callable by contract owner
+     * @param _maximumLatency Maximum Chainlink price latency
+     */
+    function setMaximumLatency(uint256 _maximumLatency) external onlyOwner {
+        if (_maximumLatency > 3600) revert LatencyToleranceTooHigh();
+        maximumLatency = _maximumLatency;
+        emit MaximumLatencyUpdated(_maximumLatency);
+    }
+
+    /**
+     * @notice Set an NFT collection's Chainlink price feed address.
+     * @dev Function only callable by contract owner
+     * @param _collection NFT collection address
+     * @param _priceFeed Chainlink price feed address
+     */
+    function setPriceFeed(address _collection, address _priceFeed) external onlyOwner {
+        priceFeeds[_collection] = _priceFeed;
+        emit PriceFeedUpdated(_collection, _priceFeed);
+    }
+}
---
+++ test/foundry/FloorBasedCollectionOrders.t.sol
@@ -0,0 +1,407 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IOwnableTwoSteps} from "@looksrare/contracts-libs/contracts/interfaces/IOwnableTwoSteps.sol";
+import {OrderStructs} from "../../contracts/libraries/OrderStructs.sol";
+import {IExecutionStrategy} from "../../contracts/interfaces/IExecutionStrategy.sol";
+import {IStrategyManager} from "../../contracts/interfaces/IStrategyManager.sol";
+import {StrategyFloorBasedCollectionOffer} from "../../contracts/executionStrategies/StrategyFloorBasedCollectionOffer.sol";
+import {ProtocolBase} from "./ProtocolBase.t.sol";
+
+contract FloorBasedCollectionOrdersTest is ProtocolBase, IStrategyManager {
+    string private constant MAINNET_RPC_URL = "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161";
+    StrategyFloorBasedCollectionOffer public strategyFloorBasedCollectionOffer;
+    // At block 15740567
+    // roundId         uint80  : 18446744073709552305
+    // answer          int256  : 9700000000000000000
+    // startedAt       uint256 : 1666100016
+    // updatedAt       uint256 : 1666100016
+    // answeredInRound uint80  : 18446744073709552305
+    uint256 private constant FORKED_BLOCK_NUMBER = 7791270;
+    uint256 private constant LATEST_CHAINLINK_ANSWER_IN_WAD = 9700000000000000000;
+    address private constant AZUKI_PRICE_FEED = 0x9F6d70CDf08d893f0063742b51d3E9D1e18b7f74;
+
+    function setUp() public override {
+        vm.createSelectFork(MAINNET_RPC_URL, FORKED_BLOCK_NUMBER);
+        super.setUp();
+
+        _setUpUsers();
+        _setUpNewStrategy();
+        _setUpRoyalties(address(mockERC721), _standardRoyaltyFee);
+    }
+
+    function _setUpNewStrategy() private asPrankedUser(_owner) {
+        strategyFloorBasedCollectionOffer = new StrategyFloorBasedCollectionOffer(address(looksRareProtocol));
+        looksRareProtocol.addStrategy(true, _standardProtocolFee, 300, address(strategyFloorBasedCollectionOffer));
+    }
+
+    function _createMakerBidAndTakerAsk(uint256 discount)
+        private
+        returns (OrderStructs.MakerBid memory makerBid, OrderStructs.TakerAsk memory takerAsk)
+    {
+        mockERC721.mint(takerUser, 1);
+
+        uint16 minNetRatio = 10000 - (_standardRoyaltyFee + _standardProtocolFee); // 3% slippage protection
+
+        // Prepare the order hash
+        makerBid = _createSingleItemMakerBidOrder({
+            bidNonce: 0,
+            subsetNonce: 0,
+            strategyId: 2,
+            assetType: 0,
+            orderNonce: 0,
+            minNetRatio: minNetRatio,
+            collection: address(mockERC721),
+            currency: address(weth),
+            signer: makerUser,
+            maxPrice: 9.5 ether,
+            itemId: 0 // Doesn't matter, not used
+        });
+
+        // makerBid.itemIds = itemIds;
+        // makerBid.amounts = amounts;
+        makerBid.additionalParameters = abi.encode(discount);
+
+        uint256[] memory itemIds = new uint256[](1);
+        itemIds[0] = 1;
+
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = 1;
+
+        takerAsk = OrderStructs.TakerAsk({
+            recipient: takerUser,
+            minNetRatio: makerAsk.minNetRatio,
+            minPrice: 9.5 ether,
+            itemIds: itemIds,
+            amounts: amounts,
+            additionalParameters: abi.encode()
+        });
+    }
+
+    function testNewStrategy() public {
+        Strategy memory strategy = looksRareProtocol.strategyInfo(2);
+        assertTrue(strategy.isActive);
+        assertTrue(strategy.hasRoyalties);
+        assertEq(strategy.protocolFee, _standardProtocolFee);
+        assertEq(strategy.maxProtocolFee, uint16(300));
+        assertEq(strategy.implementation, address(strategyFloorBasedCollectionOffer));
+    }
+
+    event MaximumLatencyUpdated(uint256 maximumLatency);
+
+    function testSetMaximumLatency() public {
+        StrategyFloorBasedCollectionOffer strategy = StrategyFloorBasedCollectionOffer(
+            looksRareProtocol.strategyInfo(2).implementation
+        );
+
+        vm.expectEmit(true, false, false, true);
+        emit MaximumLatencyUpdated(3600);
+        vm.prank(_owner);
+        strategy.setMaximumLatency(3600);
+
+        assertEq(strategy.maximumLatency(), 3600);
+    }
+
+    function testSetMaximumLatencyLatencyToleranceTooHigh() public {
+        StrategyFloorBasedCollectionOffer strategy = StrategyFloorBasedCollectionOffer(
+            looksRareProtocol.strategyInfo(2).implementation
+        );
+
+        vm.expectRevert(StrategyFloorBasedCollectionOffer.LatencyToleranceTooHigh.selector);
+        vm.prank(_owner);
+        strategy.setMaximumLatency(3601);
+    }
+
+    function testSetMaximumLatencyNotOwner() public {
+        StrategyFloorBasedCollectionOffer strategy = StrategyFloorBasedCollectionOffer(
+            looksRareProtocol.strategyInfo(2).implementation
+        );
+
+        vm.expectRevert(IOwnableTwoSteps.NotOwner.selector);
+        strategy.setMaximumLatency(3600);
+    }
+
+    function testFloorBasedCollectionOfferDesiredDiscountedPriceGreaterThanOrEqualToMaxPrice() public {
+        StrategyFloorBasedCollectionOffer strategy = StrategyFloorBasedCollectionOffer(
+            looksRareProtocol.strategyInfo(2).implementation
+        );
+
+        // Floor price = 9.7 ETH, discount = 0.3 ETH, desired price = 9.6 ETH
+        // Max price = 9.5 ETH
+        (OrderStructs.MakerBid memory makerBid, OrderStructs.TakerAsk memory takerAsk) = _createMakerBidAndTakerAsk({
+            discount: 0.1 ether
+        });
+
+        signature = _signMakerBid(makerBid, makerUserPK);
+
+        vm.startPrank(_owner);
+        strategy.setMaximumLatency(3600);
+        strategy.setPriceFeed(address(mockERC721), AZUKI_PRICE_FEED);
+        vm.stopPrank();
+
+        vm.prank(takerUser);
+        // Execute taker bid transaction
+        looksRareProtocol.executeTakerAsk(
+            takerAsk,
+            makerBid,
+            signature,
+            _emptyMerkleRoot,
+            _emptyMerkleProof,
+            _emptyReferrer
+        );
+
+        // Maker user has received the asset
+        assertEq(mockERC721.ownerOf(1), makerUser);
+
+        // Maker bid user pays the whole price
+        assertEq(weth.balanceOf(makerUser), _initialWETHBalanceUser - 9.5 ether);
+        // Taker ask user receives 97% of the whole price (2% protocol + 1% royalties)
+        assertEq(weth.balanceOf(takerUser), _initialWETHBalanceUser + 9.215 ether);
+    }
+
+    function testFloorBasedCollectionOfferDesiredDiscountedPriceLessThanMaxPrice() public {
+        StrategyFloorBasedCollectionOffer strategy = StrategyFloorBasedCollectionOffer(
+            looksRareProtocol.strategyInfo(2).implementation
+        );
+
+        // Floor price = 9.7 ETH, discount = 0.3 ETH, desired price = 9.4 ETH
+        // Max price = 9.5 ETH
+        (OrderStructs.MakerBid memory makerBid, OrderStructs.TakerAsk memory takerAsk) = _createMakerBidAndTakerAsk({
+            discount: 0.3 ether
+        });
+
+        takerAsk.minPrice = 9.4 ether;
+
+        signature = _signMakerBid(makerBid, makerUserPK);
+
+        vm.startPrank(_owner);
+        strategy.setMaximumLatency(3600);
+        strategy.setPriceFeed(address(mockERC721), AZUKI_PRICE_FEED);
+        vm.stopPrank();
+
+        vm.prank(takerUser);
+        // Execute taker bid transaction
+        looksRareProtocol.executeTakerAsk(
+            takerAsk,
+            makerBid,
+            signature,
+            _emptyMerkleRoot,
+            _emptyMerkleProof,
+            _emptyReferrer
+        );
+
+        // Maker user has received the asset
+        assertEq(mockERC721.ownerOf(1), makerUser);
+
+        // Maker bid user pays the whole price
+        assertEq(weth.balanceOf(makerUser), _initialWETHBalanceUser - 9.4 ether);
+        // Taker ask user receives 97% of the whole price (2% protocol + 1% royalties)
+        assertEq(weth.balanceOf(takerUser), _initialWETHBalanceUser + 9.118 ether);
+    }
+
+    function testPriceFeedNotAvailable() public {
+        StrategyFloorBasedCollectionOffer strategy = StrategyFloorBasedCollectionOffer(
+            looksRareProtocol.strategyInfo(2).implementation
+        );
+
+        (OrderStructs.MakerBid memory makerBid, OrderStructs.TakerAsk memory takerAsk) = _createMakerBidAndTakerAsk({
+            discount: 0.1 ether
+        });
+
+        signature = _signMakerBid(makerBid, makerUserPK);
+
+        vm.startPrank(_owner);
+        strategy.setMaximumLatency(3600);
+        vm.stopPrank();
+
+        vm.expectRevert(StrategyFloorBasedCollectionOffer.PriceFeedNotAvailable.selector);
+        vm.prank(takerUser);
+        // Execute taker bid transaction
+        looksRareProtocol.executeTakerAsk(
+            takerAsk,
+            makerBid,
+            signature,
+            _emptyMerkleRoot,
+            _emptyMerkleProof,
+            _emptyReferrer
+        );
+    }
+
+    function testOraclePriceNotRecentEnough() public {
+        StrategyFloorBasedCollectionOffer strategy = StrategyFloorBasedCollectionOffer(
+            looksRareProtocol.strategyInfo(2).implementation
+        );
+
+        (OrderStructs.MakerBid memory makerBid, OrderStructs.TakerAsk memory takerAsk) = _createMakerBidAndTakerAsk({
+            discount: 0.1 ether
+        });
+
+        signature = _signMakerBid(makerBid, makerUserPK);
+
+        vm.startPrank(_owner);
+        strategy.setPriceFeed(address(mockERC721), AZUKI_PRICE_FEED);
+        vm.stopPrank();
+
+        vm.expectRevert(StrategyFloorBasedCollectionOffer.PriceNotRecentEnough.selector);
+        vm.prank(takerUser);
+        // Execute taker bid transaction
+        looksRareProtocol.executeTakerAsk(
+            takerAsk,
+            makerBid,
+            signature,
+            _emptyMerkleRoot,
+            _emptyMerkleProof,
+            _emptyReferrer
+        );
+    }
+
+    function testCallerNotLooksRareProtocol() public {
+        StrategyFloorBasedCollectionOffer strategy = StrategyFloorBasedCollectionOffer(
+            looksRareProtocol.strategyInfo(2).implementation
+        );
+
+        (OrderStructs.MakerBid memory makerBid, OrderStructs.TakerAsk memory takerAsk) = _createMakerBidAndTakerAsk({
+            discount: 0.1 ether
+        });
+
+        vm.startPrank(_owner);
+        strategy.setMaximumLatency(3600);
+        strategy.setPriceFeed(address(mockERC721), AZUKI_PRICE_FEED);
+        vm.stopPrank();
+
+        vm.prank(takerUser);
+        vm.expectRevert(IExecutionStrategy.WrongCaller.selector);
+        // Call the function directly
+        strategyFloorBasedCollectionOffer.executeStrategyWithTakerAsk(takerAsk, makerBid);
+    }
+
+    function testTakerAskItemIdsLengthNotOne() public {
+        StrategyFloorBasedCollectionOffer strategy = StrategyFloorBasedCollectionOffer(
+            looksRareProtocol.strategyInfo(2).implementation
+        );
+
+        (OrderStructs.MakerBid memory makerBid, OrderStructs.TakerAsk memory takerAsk) = _createMakerBidAndTakerAsk({
+            discount: 0.1 ether
+        });
+
+        uint256[] memory itemIds = new uint256[](0);
+        takerAsk.itemIds = itemIds;
+
+        signature = _signMakerBid(makerBid, makerUserPK);
+
+        vm.startPrank(_owner);
+        strategy.setMaximumLatency(3600);
+        strategy.setPriceFeed(address(mockERC721), AZUKI_PRICE_FEED);
+        vm.stopPrank();
+
+        vm.prank(takerUser);
+        vm.expectRevert(IExecutionStrategy.OrderInvalid.selector);
+        // Execute taker bid transaction
+        looksRareProtocol.executeTakerAsk(
+            takerAsk,
+            makerBid,
+            signature,
+            _emptyMerkleRoot,
+            _emptyMerkleProof,
+            _emptyReferrer
+        );
+    }
+
+    function testTakerAskAmountsLengthNotOne() public {
+        StrategyFloorBasedCollectionOffer strategy = StrategyFloorBasedCollectionOffer(
+            looksRareProtocol.strategyInfo(2).implementation
+        );
+
+        (OrderStructs.MakerBid memory makerBid, OrderStructs.TakerAsk memory takerAsk) = _createMakerBidAndTakerAsk({
+            discount: 0.1 ether
+        });
+
+        uint256[] memory amounts = new uint256[](0);
+        takerAsk.amounts = amounts;
+
+        signature = _signMakerBid(makerBid, makerUserPK);
+
+        vm.startPrank(_owner);
+        strategy.setMaximumLatency(3600);
+        strategy.setPriceFeed(address(mockERC721), AZUKI_PRICE_FEED);
+        vm.stopPrank();
+
+        vm.prank(takerUser);
+        vm.expectRevert(IExecutionStrategy.OrderInvalid.selector);
+        // Execute taker bid transaction
+        looksRareProtocol.executeTakerAsk(
+            takerAsk,
+            makerBid,
+            signature,
+            _emptyMerkleRoot,
+            _emptyMerkleProof,
+            _emptyReferrer
+        );
+    }
+
+    function testTakerAskZeroAmount() public {
+        StrategyFloorBasedCollectionOffer strategy = StrategyFloorBasedCollectionOffer(
+            looksRareProtocol.strategyInfo(2).implementation
+        );
+
+        (OrderStructs.MakerBid memory makerBid, OrderStructs.TakerAsk memory takerAsk) = _createMakerBidAndTakerAsk({
+            discount: 0.1 ether
+        });
+
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = 0;
+        takerAsk.amounts = amounts;
+
+        signature = _signMakerBid(makerBid, makerUserPK);
+
+        vm.startPrank(_owner);
+        strategy.setMaximumLatency(3600);
+        strategy.setPriceFeed(address(mockERC721), AZUKI_PRICE_FEED);
+        vm.stopPrank();
+
+        vm.prank(takerUser);
+        vm.expectRevert(IExecutionStrategy.OrderInvalid.selector);
+        // Execute taker bid transaction
+        looksRareProtocol.executeTakerAsk(
+            takerAsk,
+            makerBid,
+            signature,
+            _emptyMerkleRoot,
+            _emptyMerkleProof,
+            _emptyReferrer
+        );
+    }
+
+    function testMakerBidTooLow() public {
+        StrategyFloorBasedCollectionOffer strategy = StrategyFloorBasedCollectionOffer(
+            looksRareProtocol.strategyInfo(2).implementation
+        );
+
+        // Floor price = 9.7 ETH, discount = 0.3 ETH, desired price = 9.4 ETH
+        // Maker bid max price = 9.4 ETH
+        // Taker ask min price = 9.5 ETH
+        (OrderStructs.MakerBid memory makerBid, OrderStructs.TakerAsk memory takerAsk) = _createMakerBidAndTakerAsk({
+            discount: 0.3 ether
+        });
+        makerBid.maxPrice = 9.4 ether;
+
+        signature = _signMakerBid(makerBid, makerUserPK);
+
+        vm.startPrank(_owner);
+        strategy.setMaximumLatency(3600);
+        strategy.setPriceFeed(address(mockERC721), AZUKI_PRICE_FEED);
+        vm.stopPrank();
+
+        vm.expectRevert(IExecutionStrategy.BidTooLow.selector);
+        vm.prank(takerUser);
+        // Execute taker bid transaction
+        looksRareProtocol.executeTakerAsk(
+            takerAsk,
+            makerBid,
+            signature,
+            _emptyMerkleRoot,
+            _emptyMerkleProof,
+            _emptyReferrer
+        );
+    }
+}

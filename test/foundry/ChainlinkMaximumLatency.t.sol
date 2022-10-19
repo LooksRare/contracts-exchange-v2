fc0378d7c96fc4700e00a54488b6c35dcf527b6a
hiroshi <hiroshi@looksrare.org>
2022-10-19 11:21:24 +0100

    test: Remove IStrategy

--- test/foundry/ChainlinkMaximumLatency.t.sol
+++ test/foundry/ChainlinkMaximumLatency.t.sol
@@ -2,21 +2,14 @@
 pragma solidity ^0.8.0;
 
 import {IOwnableTwoSteps} from "@looksrare/contracts-libs/contracts/interfaces/IOwnableTwoSteps.sol";
+import {StrategyChainlinkPriceLatency} from "../../contracts/executionStrategies/StrategyChainlinkPriceLatency.sol";
 import {ProtocolBase} from "./ProtocolBase.t.sol";
-
-interface IStrategy {
-    error LatencyToleranceTooHigh();
-
-    function maximumLatency() external view returns (uint256);
-
-    function setMaximumLatency(uint256 maximumLatency) external;
-}
 
 contract ChainlinkMaximumLatencyTest is ProtocolBase {
     event MaximumLatencyUpdated(uint256 maximumLatency);
 
     function _testSetMaximumLatency(address _strategy) internal {
-        IStrategy strategy = IStrategy(_strategy);
+        StrategyChainlinkPriceLatency strategy = StrategyChainlinkPriceLatency(_strategy);
 
         vm.expectEmit(true, false, false, true);
         emit MaximumLatencyUpdated(3600);
@@ -27,15 +20,15 @@ contract ChainlinkMaximumLatencyTest is ProtocolBase {
     }
 
     function _testSetMaximumLatencyLatencyToleranceTooHigh(address _strategy) internal {
-        IStrategy strategy = IStrategy(_strategy);
+        StrategyChainlinkPriceLatency strategy = StrategyChainlinkPriceLatency(_strategy);
 
-        vm.expectRevert(IStrategy.LatencyToleranceTooHigh.selector);
+        vm.expectRevert(StrategyChainlinkPriceLatency.LatencyToleranceTooHigh.selector);
         vm.prank(_owner);
         strategy.setMaximumLatency(3601);
     }
 
     function _testSetMaximumLatencyNotOwner(address _strategy) internal {
-        IStrategy strategy = IStrategy(_strategy);
+        StrategyChainlinkPriceLatency strategy = StrategyChainlinkPriceLatency(_strategy);
 
         vm.expectRevert(IOwnableTwoSteps.NotOwner.selector);
         strategy.setMaximumLatency(3600);

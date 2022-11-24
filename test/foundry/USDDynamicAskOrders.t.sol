2e2450189d7e789f2327f9acd246a146dbabe344
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-11-24 12:41:22 +0900

    test: Minor updates

--- test/foundry/USDDynamicAskOrders.t.sol
+++ test/foundry/USDDynamicAskOrders.t.sol
@@ -10,7 +10,6 @@ import {ProtocolBase} from "./ProtocolBase.t.sol";
 import {ChainlinkMaximumLatencyTest} from "./ChainlinkMaximumLatency.t.sol";
 
 contract USDDynamicAskOrdersTest is ProtocolBase, IStrategyManager, ChainlinkMaximumLatencyTest {
-    string private constant MAINNET_RPC_URL = "https://rpc.ankr.com/eth";
     StrategyUSDDynamicAsk public strategyUSDDynamicAsk;
     bytes4 public selectorTakerAsk = _emptyBytes4;
     bytes4 public selectorTakerBid = StrategyUSDDynamicAsk.executeStrategyWithTakerBid.selector;

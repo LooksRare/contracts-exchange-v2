7d7cd556df15d27557b975ffcd8cc4a17585066f
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-11-29 13:52:42 +0900

    refactor: Remove collection offer from inherited strategies

--- contracts/ExecutionManager.sol
+++ contracts/ExecutionManager.sol
@@ -171,9 +171,6 @@ contract ExecutionManager is InheritedStrategies, NonceManager, StrategyManager,
         if (makerAsk.strategyId == 0) {
             (price, itemIds, amounts) = _executeStandardSaleStrategyWithTakerBid(takerBid, makerAsk);
             isNonceInvalidated = true;
-        } else if (makerAsk.strategyId == 1) {
-            // Collection offer is not available for taker bid
-            revert StrategyNotAvailable(makerAsk.strategyId);
         } else {
             if (strategyInfo[makerAsk.strategyId].isActive) {
                 bytes4 selector = strategyInfo[makerAsk.strategyId].selectorTakerBid;
@@ -210,9 +207,6 @@ contract ExecutionManager is InheritedStrategies, NonceManager, StrategyManager,
 
         if (makerBid.strategyId == 0) {
             (price, itemIds, amounts) = _executeStandardSaleStrategyWithTakerAsk(takerAsk, makerBid);
-            isNonceInvalidated = true;
-        } else if (makerBid.strategyId == 1) {
-            (price, itemIds, amounts) = _executeCollectionStrategyWithTakerAsk(takerAsk, makerBid);
             isNonceInvalidated = true;
         } else {
             if (strategyInfo[makerBid.strategyId].isActive) {
--- contracts/InheritedStrategies.sol
+++ contracts/InheritedStrategies.sol
@@ -1,23 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-// OpenZeppelin's library for verifying Merkle proofs
-import {MerkleProof} from "./libraries/OpenZeppelin/MerkleProof.sol";
-
 // Libraries
 import {OrderStructs} from "./libraries/OrderStructs.sol";
-
-// Interfaces
-import {IInheritedStrategies} from "./interfaces/IInheritedStrategies.sol";
 
 /**
  * @title InheritedStrategies
  * @notice This contract handles the execution of inherited strategies.
  *         - StrategyId = 0 --> Standard Order
- *         - StrategyId = 1 --> Collection Order (Maker Bid only) with an option to set a merkle root for a set of criteria
  * @author LooksRare protocol team (👀,💎)
  */
-contract InheritedStrategies is IInheritedStrategies {
+contract InheritedStrategies {
+    // Custom errors
+    error OrderInvalid();
+
     /**
      * @notice Execute standard sale strategy with taker bid order
      * @param takerBid Taker bid struct (contains the taker bid-specific parameters for the execution of the transaction)
@@ -85,45 +81,6 @@ contract InheritedStrategies is IInheritedStrategies {
                     ++i;
                 }
             }
-        }
-    }
-
-    /**
-     * @notice Execute collection strategy with taker ask order
-     * @param takerAsk Taker ask struct (contains the taker ask-specific parameters for the execution of the transaction)
-     * @param makerBid Maker bid struct (contains the maker bid-specific parameters for the execution of the transaction)
-     */
-    function _executeCollectionStrategyWithTakerAsk(
-        OrderStructs.TakerAsk calldata takerAsk,
-        OrderStructs.MakerBid calldata makerBid
-    ) internal pure returns (uint256 price, uint256[] calldata itemIds, uint256[] calldata amounts) {
-        price = makerBid.maxPrice;
-        itemIds = takerAsk.itemIds;
-        amounts = makerBid.amounts;
-
-        // A collection order can only be executable for 1 itemId but quantity to fill can vary
-        {
-            if (
-                itemIds.length != 1 ||
-                amounts.length != 1 ||
-                price != takerAsk.minPrice ||
-                takerAsk.amounts[0] != amounts[0] ||
-                amounts[0] == 0
-            ) revert OrderInvalid();
-        }
-
-        if (makerBid.additionalParameters.length != 0) {
-            // Precomputed merkleRoot (that contains the itemIds that match a common characteristic)
-            bytes32 root = abi.decode(makerBid.additionalParameters, (bytes32));
-
-            // MerkleProof + indexInTree + itemId
-            bytes32[] memory proof = abi.decode(takerAsk.additionalParameters, (bytes32[]));
-
-            // Compute the node
-            bytes32 node = keccak256(abi.encodePacked(takerAsk.itemIds[0]));
-
-            // Verify proof
-            if (!MerkleProof.verify(proof, root, node)) revert OrderMerkleProofInvalid();
         }
     }
 }

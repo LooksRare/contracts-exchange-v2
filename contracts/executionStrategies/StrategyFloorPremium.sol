412c3e140e6d8fe5f4196babcce14063f41e1779
0xhiroshi <108636367+0xhiroshi@users.noreply.github.com>
2022-11-30 09:08:53 +0000

    feat: Premium as a percentage (#4)
    * feat: Premium as a percentage
    
    * refactor: FloorPremiumOrdersTest abstract contract
    
    * build: Run yarn format:write
    
    * fix: Strategy is a struct
    
    * fix: Func isValid only accepts 1 arg
    
    * feat: Split fixed premium function
    
    * feat: Split percentage premium function
    
    * docs: Update Natspec for executePercentagePremiumStrategyWithTakerBid
    
    * refactor: Update logic

--- contracts/executionStrategies/StrategyFloorPremium.sol
+++ contracts/executionStrategies/StrategyFloorPremium.sol
@@ -34,7 +34,7 @@ contract StrategyFloorPremium is StrategyChainlinkMultiplePriceFeeds, StrategyCh
      * @param makerAsk Maker ask struct (contains the maker ask-specific parameters for the execution of the transaction)
      * @dev The client has to provide the bidder's desired premium amount in ETH from the floor price as the additionalParameters.
      */
-    function executeStrategyWithTakerBid(
+    function executeFixedPremiumStrategyWithTakerBid(
         OrderStructs.TakerBid calldata takerBid,
         OrderStructs.MakerAsk calldata makerAsk
     )
@@ -59,19 +59,66 @@ contract StrategyFloorPremium is StrategyChainlinkMultiplePriceFeeds, StrategyCh
         if (answer <= 0) revert InvalidChainlinkPrice();
         if (block.timestamp > maximumLatency + updatedAt) revert PriceNotRecentEnough();
 
-        uint256 premiumAmount = abi.decode(makerAsk.additionalParameters, (uint256));
+        uint256 premium = abi.decode(makerAsk.additionalParameters, (uint256));
         uint256 floorPrice = uint256(answer);
-        uint256 desiredPrice = floorPrice + premiumAmount;
-
-        if (takerBid.maxPrice < desiredPrice) {
-            if (takerBid.maxPrice < makerAsk.minPrice) revert BidTooLow();
-        }
+        uint256 desiredPrice = floorPrice + premium;
 
         if (desiredPrice >= makerAsk.minPrice) {
             price = desiredPrice;
         } else {
             price = makerAsk.minPrice;
         }
+
+        if (takerBid.maxPrice < desiredPrice) revert BidTooLow();
+
+        itemIds = makerAsk.itemIds;
+        amounts = makerAsk.amounts;
+        isNonceInvalidated = true;
+    }
+
+    /**
+     * @notice Validate the order under the context of the chosen strategy and return the fulfillable items/amounts/price/nonce invalidation status
+     *         This strategy looks at the seller's desired execution price in ETH (floor * (1 + premium)) and minimum execution price and chooses the higher price
+     * @param takerBid Taker bid struct (contains the taker bid-specific parameters for the execution of the transaction)
+     * @param makerAsk Maker ask struct (contains the maker ask-specific parameters for the execution of the transaction)
+     * @dev The client has to provide the bidder's desired premium amount in ETH from the floor price as the additionalParameters.
+     */
+    function executePercentagePremiumStrategyWithTakerBid(
+        OrderStructs.TakerBid calldata takerBid,
+        OrderStructs.MakerAsk calldata makerAsk
+    )
+        external
+        view
+        returns (uint256 price, uint256[] memory itemIds, uint256[] memory amounts, bool isNonceInvalidated)
+    {
+        if (msg.sender != LOOKSRARE_PROTOCOL) revert WrongCaller();
+
+        if (
+            makerAsk.itemIds.length != 1 ||
+            makerAsk.amounts.length != 1 ||
+            makerAsk.amounts[0] != 1 ||
+            makerAsk.itemIds[0] != takerBid.itemIds[0] ||
+            takerBid.amounts[0] != 1
+        ) revert OrderInvalid();
+
+        address priceFeed = priceFeeds[makerAsk.collection];
+        if (priceFeed == address(0)) revert PriceFeedNotAvailable();
+
+        (, int256 answer, , uint256 updatedAt, ) = AggregatorV3Interface(priceFeed).latestRoundData();
+        if (answer <= 0) revert InvalidChainlinkPrice();
+        if (block.timestamp > maximumLatency + updatedAt) revert PriceNotRecentEnough();
+
+        uint256 premium = abi.decode(makerAsk.additionalParameters, (uint256));
+        uint256 floorPrice = uint256(answer);
+        uint256 desiredPrice = (floorPrice * (10_000 + premium)) / 10_000;
+
+        if (desiredPrice >= makerAsk.minPrice) {
+            price = desiredPrice;
+        } else {
+            price = makerAsk.minPrice;
+        }
+
+        if (takerBid.maxPrice < desiredPrice) revert BidTooLow();
 
         itemIds = makerAsk.itemIds;
         amounts = makerAsk.amounts;

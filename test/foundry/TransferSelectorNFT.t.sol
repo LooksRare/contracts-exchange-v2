74f4e52f4ad436d3f51a896f0bb8580341efad3f
hiroshi <hiroshi@looksrare.org>
2022-12-07 17:37:24 +0000

    test: TransferSelectorNFT initial states

--- test/foundry/TransferSelectorNFT.t.sol
+++ test/foundry/TransferSelectorNFT.t.sol
@@ -9,12 +9,24 @@ contract TransferSelectorNFTTest is ProtocolBase, ITransferSelectorNFT {
     address newTransferManager = address(69_420);
     bytes4 newSelector = 0x69696969;
 
+    function testInitialStates() public {
+        (address transferManager0, bytes4 selector0) = looksRareProtocol.managerSelectorOfAssetType(uint8(0));
+        assertEq(transferManager0, address(transferManager));
+        assertEq(uint32(selector0), uint32(0xa7bc96d3));
+
+        (address transferManager1, bytes4 selector1) = looksRareProtocol.managerSelectorOfAssetType(uint8(1));
+        assertEq(transferManager1, address(transferManager));
+        assertEq(uint32(selector1), uint32(0xa0a406c6));
+    }
+
     function testAddTransferManagerForAssetType() public asPrankedUser(_owner) {
         vm.expectEmit(true, false, false, true);
         emit NewAssetType(uint8(2), newTransferManager, newSelector);
         looksRareProtocol.addTransferManagerForAssetType(uint8(2), newTransferManager, newSelector);
 
         (address transferManager, bytes4 selector) = looksRareProtocol.managerSelectorOfAssetType(uint8(2));
+        assertEq(transferManager, newTransferManager);
+        assertEq(uint32(selector), uint32(newSelector));
     }
 
     function testAddTransferManagerForAssetTypeNotOwner() public {

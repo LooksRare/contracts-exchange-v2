e6b5eb9b1b7b7e858493ace72e3d4efcfedc9055
hiroshi <hiroshi@looksrare.org>
2022-12-07 17:29:14 +0000

    test: TransferSelectorNFT addTransferManagerForAssetType tests

---
+++ test/foundry/TransferSelectorNFT.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {ProtocolBase} from "./ProtocolBase.t.sol";
+import {IOwnableTwoSteps} from "@looksrare/contracts-libs/contracts/interfaces/IOwnableTwoSteps.sol";
+import {ITransferSelectorNFT} from "../../contracts/interfaces/ITransferSelectorNFT.sol";
+
+contract TransferSelectorNFTTest is ProtocolBase, ITransferSelectorNFT {
+    address newTransferManager = address(69_420);
+    bytes4 newSelector = 0x69696969;
+
+    function testAddTransferManagerForAssetType() public asPrankedUser(_owner) {
+        vm.expectEmit(true, false, false, true);
+        emit NewAssetType(uint8(2), newTransferManager, newSelector);
+        looksRareProtocol.addTransferManagerForAssetType(uint8(2), newTransferManager, newSelector);
+
+        (address transferManager, bytes4 selector) = looksRareProtocol.managerSelectorOfAssetType(uint8(2));
+    }
+
+    function testAddTransferManagerForAssetTypeNotOwner() public {
+        vm.expectRevert(IOwnableTwoSteps.NotOwner.selector);
+        looksRareProtocol.addTransferManagerForAssetType(2, newTransferManager, newSelector);
+    }
+
+    function testAddTransferManagerForAssetTypeAlreadySet() public asPrankedUser(_owner) {
+        vm.expectRevert(AlreadySet.selector);
+        looksRareProtocol.addTransferManagerForAssetType(0, newTransferManager, newSelector);
+    }
+}

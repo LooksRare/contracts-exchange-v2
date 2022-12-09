d6bea359c5dde57caf19ce094f0ae1cb27461058
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-12-09 17:23:26 +0900

    chore: Helpers

---
+++ contracts/helpers/ProtocolHelpers.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+// LooksRare unopinionated libraries
+import {SignatureChecker} from "@looksrare/contracts-libs/contracts/SignatureChecker.sol";
+
+// Libraries
+import {OrderStructs} from "../libraries/OrderStructs.sol";
+
+// Other dependencies
+import {LooksRareProtocol} from "../LooksRareProtocol.sol";
+
+/**
+ * @title ProtocolHelpers
+ * @notice This contract contains helper view functions for order creation.
+ * @author LooksRare protocol team (👀,💎)
+ */
+contract ProtocolHelpers {
+    using OrderStructs for OrderStructs.MakerAsk;
+    using OrderStructs for OrderStructs.MakerBid;
+    using OrderStructs for OrderStructs.MerkleTree;
+
+    // Encoding prefix for EIP-712 signatures
+    string internal constant _ENCODING_PREFIX = "\x19\x01";
+
+    // LooksRareProtocol
+    LooksRareProtocol public looksRareProtocol;
+
+    /**
+     * @notice Constructor
+     * @param _looksRareProtocol LooksRare protocol address
+     */
+    constructor(address _looksRareProtocol) {
+        looksRareProtocol = LooksRareProtocol(_looksRareProtocol);
+    }
+
+    /**
+     * @notice Compute digest for maker ask
+     * @param makerAsk Maker ask struct
+     * @return digest Digest
+     */
+    function computeDigestMakerAsk(OrderStructs.MakerAsk memory makerAsk) public view returns (bytes32 digest) {
+        bytes32 domainSeparator = looksRareProtocol.domainSeparator();
+        return keccak256(abi.encodePacked(_ENCODING_PREFIX, domainSeparator, makerAsk.hash()));
+    }
+
+    /**
+     * @notice Compute digest for maker bid
+     * @param makerBid Maker bid struct
+     * @return digest Digest
+     */
+    function computeDigestMakerBid(OrderStructs.MakerBid memory makerBid) public view returns (bytes32 digest) {
+        bytes32 domainSeparator = looksRareProtocol.domainSeparator();
+        return keccak256(abi.encodePacked(_ENCODING_PREFIX, domainSeparator, makerBid.hash()));
+    }
+
+    /**
+     * @notice Compute digest for merkle tree
+     * @param merkleTree Merkle tree struct
+     */
+    function computeDigestMerkleTree(OrderStructs.MerkleTree memory merkleTree) public view returns (bytes32 digest) {
+        bytes32 domainSeparator = looksRareProtocol.domainSeparator();
+        return keccak256(abi.encodePacked(_ENCODING_PREFIX, domainSeparator, merkleTree.hash()));
+    }
+
+    /**
+     * @notice Verify maker ask order
+     * @param makerAsk Maker ask struct
+     * @param makerSignature Maker signature
+     * @param signer Signer address
+     */
+    function verifyMakerAskOrder(
+        OrderStructs.MakerAsk memory makerAsk,
+        bytes calldata makerSignature,
+        address signer
+    ) public view returns (bool) {
+        bytes32 digest = computeDigestMakerAsk(makerAsk);
+        SignatureChecker.verify(digest, signer, makerSignature);
+        return true;
+    }
+
+    /**
+     * @notice Verify maker bid order
+     * @param makerBid Maker bid struct
+     * @param makerSignature Maker signature
+     * @param signer Signer address
+     */
+    function verifyMakerBidOrder(
+        OrderStructs.MakerBid memory makerBid,
+        bytes calldata makerSignature,
+        address signer
+    ) public view returns (bool) {
+        bytes32 digest = computeDigestMakerBid(makerBid);
+        SignatureChecker.verify(digest, signer, makerSignature);
+        return true;
+    }
+
+    /**
+     * @notice Verify merkle tree
+     * @param merkleTree Merkle tree struct
+     * @param makerSignature Maker signature
+     * @param signer Signer address
+     */
+    function verifyMerkleRoot(
+        OrderStructs.MerkleTree memory merkleTree,
+        bytes calldata makerSignature,
+        address signer
+    ) public view returns (bool) {
+        bytes32 digest = computeDigestMerkleTree(merkleTree);
+        SignatureChecker.verify(digest, signer, makerSignature);
+        return true;
+    }
+}

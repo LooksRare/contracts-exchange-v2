7a6f776e0c1e6ecdbee391640fccc04a4f0b5ac7
hiroshi <hiroshi@looksrare.org>
2022-12-15 11:12:24 +0000

    chore: Remove unused error

--- contracts/interfaces/INonceManager.sol
+++ contracts/interfaces/INonceManager.sol
@@ -6,9 +6,6 @@ pragma solidity ^0.8.17;
  * @author LooksRare protocol team (👀,💎)
  */
 interface INonceManager {
-    // Custom errors
-    error NoNonceToIncrement();
-
     // Events
     event NewBidAskNonces(uint112 bidNonce, uint112 askNonce);
     event OrderNoncesCancelled(uint256[] orderNonces);

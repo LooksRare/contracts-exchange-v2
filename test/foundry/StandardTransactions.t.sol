c015ec58874ba0e664d63824cb90f7038a7308d0
hiroshi <hiroshi@looksrare.org>
2022-12-15 11:21:03 +0000

    chore: Upgrade Solidity pragma

--- test/foundry/StandardTransactions.t.sol
+++ test/foundry/StandardTransactions.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 
 // Libraries and interfaces
 import {ITransferSelectorNFT} from "../../contracts/interfaces/ITransferSelectorNFT.sol";

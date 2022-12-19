0f5031a72dcb1fcfc1598b50f5c5be68884cdb2b
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-12-19 15:24:09 +0900

    build: Deployment script OrderValidator

--- scripts/deployment/DeploymentNoCreate2.sol
+++ scripts/deployment/DeploymentNoCreate2.sol
@@ -5,9 +5,9 @@ import {Test} from "../../lib/forge-std/src/Test.sol";
 
 import {LooksRareProtocol} from "../../contracts/LooksRareProtocol.sol";
 import {TransferManager} from "../../contracts/TransferManager.sol";
+import {OrderValidatorV2A} from "../../contracts/helpers/OrderValidatorV2A.sol";
 
 contract DeploymentNoCreate2 is Test {
-    // WETH
     // Custom errors
     error NoRegistryAddressSet();
     error WrongChainId(uint256 chainId);
@@ -15,6 +15,9 @@ contract DeploymentNoCreate2 is Test {
     // Contracts
     TransferManager public transferManager;
     LooksRareProtocol public looksRareProtocol;
+    OrderValidatorV2A public orderValidatorV2A;
+
+    // WETH
     address public weth;
 
     function deploy() public {
@@ -38,11 +41,14 @@ contract DeploymentNoCreate2 is Test {
         looksRareProtocol.updateCurrencyWhitelistStatus(address(0), true);
         looksRareProtocol.updateCurrencyWhitelistStatus(weth, true);
         looksRareProtocol.setProtocolFeeRecipient(looksRareProtocol.owner());
+        orderValidatorV2A = new OrderValidatorV2A(address(looksRareProtocol));
 
         console.log("TransferManager address: ");
         console.log(address(transferManager));
         console.log("LooksRareProtocol address: ");
         console.log(address(looksRareProtocol));
+        console.log("OrderValidatorV2A address: ");
+        console.log(address(orderValidatorV2A));
         vm.stopBroadcast();
     }
 }

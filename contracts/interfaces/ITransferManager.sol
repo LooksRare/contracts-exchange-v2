7ff3980e10ac734eb8c18f6bf4b25fc5cfe3e720
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-12-23 13:13:42 +0900

    docs: Natspec for more errors

--- contracts/interfaces/ITransferManager.sol
+++ contracts/interfaces/ITransferManager.sol
@@ -6,17 +6,60 @@ pragma solidity ^0.8.17;
  * @author LooksRare protocol team (👀,💎)
  */
 interface ITransferManager {
-    // Custom errors
-    error AlreadyApproved();
-    error AlreadyWhitelisted();
-    error NotApproved();
-    error NotWhitelisted();
-    error TransferCallerInvalid();
-    error WrongAssetType(uint256 assetType);
-
-    // Events
+    /**
+     * @notice It is emitted if new operators are added to the user's whitelist.
+     * @param user Address of the user
+     * @param operators Array of operator addresses
+     */
     event ApprovalsGranted(address user, address[] operators);
+
+    /**
+     * @notice It is emitted if existing operators are removed from the user's whitelist.
+     * @param user Address of the user
+     * @param operators Array of operator addresses
+     */
     event ApprovalsRemoved(address user, address[] operators);
+
+    /**
+     * @notice It is emitted if an existing operator is removed from the whitelist.
+     * @param operator Operator address
+     */
     event OperatorRemoved(address operator);
+
+    /**
+     * @notice It is emitted if a new operator is added to the whitelist.
+     * @param operator Operator address
+     */
     event OperatorWhitelisted(address operator);
+
+    /**
+     * @notice It is returned if the transfer caller is already approved by the user.
+     */
+    error AlreadyApproved();
+
+    /**
+     * @notice It is returned if the transfer caller is already whitelisted by the owner.
+     */
+    error AlreadyWhitelisted();
+
+    /**
+     * @notice It is returned if the transfer caller to approve isn't approved by the user.
+     */
+    error NotApproved();
+
+    /**
+     * @notice It is returned if the transfer caller to approve isn't whitelisted by the owner.
+     */
+    error NotWhitelisted();
+
+    /**
+     * @notice It is returned if the transfer caller is invalid
+     */
+    error TransferCallerInvalid();
+
+    /**
+     * @notice It is returned if the asset type isn't 0 or 1.
+     * @param assetType Asset type id
+     */
+    error WrongAssetType(uint256 assetType);
 }

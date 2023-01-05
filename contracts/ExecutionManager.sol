ad67b717bdd7ca5bacbd0d7b793a1a8598c6123c
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2023-01-05 15:45:59 +0900

    docs: Fix doc

--- contracts/ExecutionManager.sol
+++ contracts/ExecutionManager.sol
@@ -137,7 +137,7 @@ contract ExecutionManager is InheritedStrategy, NonceManager, StrategyManager, I
             (recipients[1], fees[1]) = creatorFeeManager.viewCreatorFeeInfo(makerBid.collection, price, itemIds);
 
             if (recipients[1] == address(0)) {
-                // If fee is null, creator fee is set at 0
+                // If recipient is null address, creator fee is set at 0
                 fees[1] = 0;
             } else if (fees[1] * 10_000 > (price * uint256(maxCreatorFeeBp))) {
                 // If creator fee is higher than tolerated, it reverts

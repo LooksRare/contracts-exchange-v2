c8dce7ad18e229c1473340080d60f7ce5104350e
hiroshi <hiroshi@looksrare.org>
2023-01-11 15:16:13 +0000

    test: Update too early to execute test

--- test/foundry/ExecutionManager.t.sol
+++ test/foundry/ExecutionManager.t.sol
@@ -70,19 +70,19 @@ contract ExecutionManagerTest is ProtocolBase, IExecutionManager, IStrategyManag
     uint256 private constant ONE_THOUSAND_YEARS = 31_536_000_000 seconds;
     uint256 private constant BEGINNING_OF_2023 = 1_672_531_200;
 
-    function testCannotValidateOrderIfTooEarlyToExecute(uint256 delta) public asPrankedUser(takerUser) {
+    function testCannotValidateOrderIfTooEarlyToExecute(uint256 timestamp) public asPrankedUser(takerUser) {
         // This logic is at least valid for the next 1,000 years
-        vm.assume(delta < ONE_THOUSAND_YEARS);
+        vm.assume(timestamp > BEGINNING_OF_2023 && timestamp < BEGINNING_OF_2023 + ONE_THOUSAND_YEARS);
         // Change timestamp to avoid underflow issues
-        vm.warp(BEGINNING_OF_2023);
+        vm.warp(timestamp);
 
         (OrderStructs.MakerBid memory makerBid, OrderStructs.TakerAsk memory takerAsk) = _createMockMakerBidAndTakerAsk(
             address(mockERC721),
             address(weth)
         );
 
-        makerBid.startTime = block.timestamp + delta;
-        makerBid.endTime = block.timestamp + delta + 1;
+        makerBid.startTime = block.timestamp;
+        makerBid.endTime = block.timestamp + 1;
 
         // Maker bid is valid if its start time is within 5 minutes into the future
         vm.warp(makerBid.startTime - 5 minutes);

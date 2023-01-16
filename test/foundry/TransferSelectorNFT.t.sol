fe57c85c99515d80863d9a1b8a239c0688e06068
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2023-01-16 12:16:31 +0900

    test: Updates

--- test/foundry/TransferSelectorNFT.t.sol
+++ test/foundry/TransferSelectorNFT.t.sol
@@ -12,26 +12,12 @@ import {ITransferSelectorNFT} from "../../contracts/interfaces/ITransferSelector
 
 // Errors
 import {ASSET_TYPE_NOT_SUPPORTED} from "../../contracts/helpers/ValidationCodeConstants.sol";
+import {WrongAssetType} from "../../contracts/interfaces/SharedErrors.sol";
 
 // Base test
 import {ProtocolBase} from "./ProtocolBase.t.sol";
 
 contract TransferSelectorNFTTest is ProtocolBase, ITransferSelectorNFT {
-    address public newTransferManager = address(69_420);
-
-    // Wrong selector
-    bytes4 public newSelector = 0x69696969;
-
-    function testInitialStates() public {
-        (address transferManager0, bytes4 selector0) = looksRareProtocol.managerSelectorOfAssetType(0);
-        assertEq(transferManager0, address(transferManager));
-        assertEq(uint32(selector0), uint32(0xa7bc96d3));
-
-        (address transferManager1, bytes4 selector1) = looksRareProtocol.managerSelectorOfAssetType(1);
-        assertEq(transferManager1, address(transferManager));
-        assertEq(uint32(selector1), uint32(0xa0a406c6));
-    }
-
     function testCannotTransferIfNoManagerSelectorForAssetType() public {
         _setUpUsers();
         uint256 price = 0.1 ether;
@@ -53,7 +39,7 @@ contract TransferSelectorNFTTest is ProtocolBase, ITransferSelectorNFT {
         _doesMakerAskOrderReturnValidationCode(makerAsk, signature, ASSET_TYPE_NOT_SUPPORTED);
 
         vm.prank(takerUser);
-        vm.expectRevert(abi.encodeWithSelector(ITransferSelectorNFT.NoTransferManagerForAssetType.selector, 2));
+        vm.expectRevert(abi.encodeWithSelector(WrongAssetType.selector, 2));
         looksRareProtocol.executeTakerBid{value: price}(
             takerBid,
             makerAsk,
@@ -61,39 +47,5 @@ contract TransferSelectorNFTTest is ProtocolBase, ITransferSelectorNFT {
             _EMPTY_MERKLE_TREE,
             _EMPTY_AFFILIATE
         );
-    }
-
-    function testAddTransferManagerForAssetType() public asPrankedUser(_owner) {
-        vm.expectEmit(true, false, false, true);
-        emit NewAssetType(2, newTransferManager, newSelector);
-        looksRareProtocol.addTransferManagerForAssetType(2, newTransferManager, newSelector);
-
-        (address currentTransferManager, bytes4 selector) = looksRareProtocol.managerSelectorOfAssetType(2);
-        assertEq(currentTransferManager, newTransferManager);
-        assertEq(uint32(selector), uint32(newSelector));
-    }
-
-    function testAddTransferManagerForAssetTypeNotOwner() public {
-        vm.expectRevert(IOwnableTwoSteps.NotOwner.selector);
-        looksRareProtocol.addTransferManagerForAssetType(2, newTransferManager, newSelector);
-    }
-
-    function testAddTransferManagerManagerSelectorAlreadySetForAssetType() public asPrankedUser(_owner) {
-        vm.expectRevert(ManagerSelectorAlreadySetForAssetType.selector);
-        looksRareProtocol.addTransferManagerForAssetType(0, newTransferManager, newSelector);
-    }
-
-    function testAddTransferManagerManagerSelectorEmpty() public asPrankedUser(_owner) {
-        // 1. Empty transfer manager address
-        vm.expectRevert(ManagerSelectorEmpty.selector);
-        looksRareProtocol.addTransferManagerForAssetType(2, address(0), newSelector);
-
-        // 2. Address transfer manager is protocol address
-        vm.expectRevert(ManagerSelectorEmpty.selector);
-        looksRareProtocol.addTransferManagerForAssetType(2, address(looksRareProtocol), newSelector);
-
-        // 3. Empty selector
-        vm.expectRevert(ManagerSelectorEmpty.selector);
-        looksRareProtocol.addTransferManagerForAssetType(2, address(1), _EMPTY_BYTES4);
     }
 }

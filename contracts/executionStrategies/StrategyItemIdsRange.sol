120adfcb8e545f247b69c4bfa80ffd9c7851d800
hiroshi <hiroshi@looksrare.org>
2023-01-25 11:06:53 +0000

    refactor: Spearbit issue 71 (StrategyItemIdsRange)

--- contracts/executionStrategies/StrategyItemIdsRange.sol
+++ contracts/executionStrategies/StrategyItemIdsRange.sol
@@ -33,8 +33,9 @@ contract StrategyItemIdsRange is BaseStrategy {
         pure
         returns (uint256 price, uint256[] memory itemIds, uint256[] memory amounts, bool isNonceInvalidated)
     {
-        uint256 length = takerAsk.itemIds.length;
-        if (length != takerAsk.amounts.length) {
+        (itemIds, amounts) = abi.decode(takerAsk.additionalParameters, (uint256[], uint256[]));
+        uint256 length = itemIds.length;
+        if (length != amounts.length) {
             revert OrderInvalid();
         }
 
@@ -51,7 +52,7 @@ contract StrategyItemIdsRange is BaseStrategy {
         uint256 lastItemId;
 
         for (uint256 i; i < length; ) {
-            uint256 offeredItemId = takerAsk.itemIds[i];
+            uint256 offeredItemId = itemIds[i];
             // Force the client to sort the item ids in ascending order,
             // in order to prevent taker ask from providing duplicated
             // item ids
@@ -61,7 +62,7 @@ contract StrategyItemIdsRange is BaseStrategy {
                 }
             }
 
-            uint256 amount = takerAsk.amounts[i];
+            uint256 amount = amounts[i];
 
             _validateAmount(amount, makerBid.assetType);
 
@@ -87,8 +88,6 @@ contract StrategyItemIdsRange is BaseStrategy {
         }
 
         price = makerBid.maxPrice;
-        itemIds = takerAsk.itemIds;
-        amounts = takerAsk.amounts;
         isNonceInvalidated = true;
     }
 

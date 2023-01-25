68b008547ef70b8de0bbbf500c1d0e3864498a93
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2023-01-25 17:10:20 +0900

    test: EIP2098

---
+++ test/foundry/SignaturesEIP2098.t.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+// Libraries, interfaces, errors
+import "@looksrare/contracts-libs/contracts/errors/SignatureCheckerErrors.sol";
+import {OrderStructs} from "../../contracts/libraries/OrderStructs.sol";
+
+// Base test
+import {ProtocolBase} from "./ProtocolBase.t.sol";
+
+// Constants
+import {ONE_HUNDRED_PERCENT_IN_BP, ASSET_TYPE_ERC721} from "../../contracts/constants/NumericConstants.sol";
+
+contract Signatures is ProtocolBase {
+    function testCanSignValidMakerAskEIP2098(uint256 price, uint256 itemId) public {
+        vm.assume(price <= 2 ether);
+
+        _setUpUsers();
+        _setupRegistryRoyalties(address(mockERC721), _standardRoyaltyFee);
+
+        // Mint asset
+        mockERC721.mint(makerUser, itemId);
+
+        (
+            OrderStructs.MakerAsk memory makerAsk,
+            ,
+            bytes memory signature
+        ) = _createSingleItemMakerAskAndTakerBidOrderAndSignature({
+                askNonce: 0,
+                subsetNonce: 0,
+                strategyId: STANDARD_SALE_FOR_FIXED_PRICE_STRATEGY,
+                assetType: ASSET_TYPE_ERC721,
+                orderNonce: 0,
+                collection: address(mockERC721),
+                currency: ETH,
+                signer: makerUser,
+                minPrice: price,
+                itemId: itemId
+            });
+
+        // Adjust the signature
+        signature = _eip2098Signature(signature);
+
+        // Verify validity of maker ask order
+        _isMakerAskOrderValid(makerAsk, signature);
+    }
+
+    function testCanSignValidMakerBidEIP2098(uint256 price, uint256 itemId) public {
+        vm.assume(price <= 2 ether);
+
+        _setUpUsers();
+        _setupRegistryRoyalties(address(mockERC721), _standardRoyaltyFee);
+
+        // Mint asset
+        mockERC721.mint(takerUser, itemId);
+
+        (
+            OrderStructs.MakerBid memory makerBid,
+            ,
+            bytes memory signature
+        ) = _createSingleItemMakerBidAndTakerAskOrderAndSignature({
+                bidNonce: 0,
+                subsetNonce: 0,
+                strategyId: STANDARD_SALE_FOR_FIXED_PRICE_STRATEGY,
+                assetType: ASSET_TYPE_ERC721,
+                orderNonce: 0,
+                collection: address(mockERC721),
+                currency: address(weth),
+                signer: makerUser,
+                maxPrice: price,
+                itemId: itemId
+            });
+
+        // Adjust the signature
+        signature = _eip2098Signature(signature);
+
+        // Verify validity of maker bid order
+        _isMakerBidOrderValid(makerBid, signature);
+    }
+}

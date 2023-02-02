7bd2d7c0cb9e50ff584ce77833a20fbe7a9ac8f1
hiroshi <hiroshi@looksrare.org>
2023-02-02 13:01:51 +0000

    chore: Arrange Chainlink errors alphabetically

--- contracts/errors/ChainlinkErrors.sol
+++ contracts/errors/ChainlinkErrors.sol
@@ -2,11 +2,6 @@
 pragma solidity ^0.8.17;
 
 /**
- * @notice It is returned if the fixed discount for a maker bid is greater than floor price.
- */
-error DiscountGreaterThanFloorPrice();
-
-/**
  * @notice It is returned if the Chainlink price is invalid (e.g. negative).
  */
 error ChainlinkPriceInvalid();
@@ -17,6 +12,11 @@ error ChainlinkPriceInvalid();
  * @dev It can only be returned for owner operations.
  */
 error DecimalsInvalid();
+
+/**
+ * @notice It is returned if the fixed discount for a maker bid is greater than floor price.
+ */
+error DiscountGreaterThanFloorPrice();
 
 /**
  * @notice It is returned if the latency tolerance is set too high (i.e. greater than 3,600 sec).

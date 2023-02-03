fc50eec43aeb2b47fc6832b2b89c139010aa6aaa
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2023-02-03 18:14:23 +0900

    fix: Pragma

--- test/foundry/executionStrategies/Reservoir/StrategyReservoirCollectionOffer.t.sol
+++ test/foundry/executionStrategies/Reservoir/StrategyReservoirCollectionOffer.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity 0.8.17;
 
 // LooksRare unopinionated libraries
 import {IERC721} from "@looksrare/contracts-libs/contracts/interfaces/generic/IERC721.sol";

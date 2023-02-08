d001e168ece4fcf651e031591ca8c27e2bd28398
hiroshi <hiroshi@looksrare.org>
2023-02-08 11:49:13 +0000

    docs: Make comment one line

--- contracts/enums/AssetType.sol
+++ contracts/enums/AssetType.sol
@@ -2,8 +2,7 @@
 pragma solidity 0.8.17;
 
 /**
- * @notice AssetType is used in OrderStructs.Maker's assetType to determine the asset
- *         type being traded.
+ * @notice AssetType is used in OrderStructs.Maker's assetType to determine the asset type being traded.
  */
 enum AssetType {
     ERC721,

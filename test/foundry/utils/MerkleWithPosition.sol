07d4f0a07b414b49c8f7ffb19e5c60002cf73f38
hiroshi <hiroshi@looksrare.org>
2023-02-10 21:39:23 +0000

    docs: Add Natspec

--- test/foundry/utils/MerkleWithPosition.sol
+++ test/foundry/utils/MerkleWithPosition.sol
@@ -3,6 +3,9 @@ pragma solidity 0.8.17;
 
 import {OrderStructs} from "../../../contracts/libraries/OrderStructs.sol";
 
+/**
+ * @dev Modified from MurkyBase to add each node's position after hashing.
+ */
 contract MerkleWithPosition {
     /**********************
      * PROOF VERIFICATION *

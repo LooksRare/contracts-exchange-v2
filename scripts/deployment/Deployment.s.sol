4fddcb14fb4baf694d1ac6b8da80a4fa365b27b2
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2023-02-10 19:08:11 +0900

    fix: Deployment script

--- scripts/deployment/Deployment.s.sol
+++ scripts/deployment/Deployment.s.sol
@@ -10,6 +10,7 @@ import {WETH} from "solmate/src/tokens/WETH.sol";
 // Core contracts
 import {LooksRareProtocol} from "../../contracts/LooksRareProtocol.sol";
 import {TransferManager} from "../../contracts/TransferManager.sol";
+import {CreatorFeeManagerWithRebates} from "../../contracts/CreatorFeeManagerWithRebates.sol";
 
 // Create2 factory interface
 import {IImmutableCreate2Factory} from "../../contracts/interfaces/IImmutableCreate2Factory.sol";
@@ -26,17 +27,20 @@ contract DeploymentNoCreate2 is Script {
     // WETH
     address public weth;
 
+    // Royalty fee registry
+    address public royaltyFeeRegistry;
+
     function _run() internal {
         uint256 chainId = block.chainId;
 
         if (chainId == 1) {
             weth = 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2;
+            royaltyFeeRegistry = 0x55010472a93921a117aAD9b055c141060c8d8022;
             uint256 deployerPrivateKey = vm.envUint("MAINNET_KEY");
         } else if (chainId == 5) {
             weth = 0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6;
+            royaltyFeeRegistry = 0x12405dB79325D06a973aD913D6e9BdA1343cD526;
             uint256 deployerPrivateKey = vm.envUint("GOERLI_KEY");
-        } else if (chainId == 1337) {
-            weth = new WETH();
         } else {
             revert ChainIdInvalid(chainId);
         }
@@ -66,18 +70,24 @@ contract DeploymentNoCreate2 is Script {
             )
         });
 
-        // 3. Deploy OrderValidatorV2A
-        orderValidatorV2A = new OrderValidatorV2A(looksRareProtocolAddress);
+        // 3. Deploy CreatorFeeManagerWithRebates
+        CreatorFeeManagerWithRebates creatorFeeManager = new CreatorFeeManagerWithRebates(royaltyFeeRegistry);
 
-        // 4. Other operations
+        // 4. Deploy OrderValidatorV2A
+        OrderValidatorV2A orderValidatorV2A = new OrderValidatorV2A(looksRareProtocolAddress);
+
+        // 5. Other operations
         transferManager.allowOperator(address(looksRareProtocol));
         looksRareProtocol.updateCurrencyStatus(address(0), true);
         looksRareProtocol.updateCurrencyStatus(weth, true);
+        looksRareProtocol.updateCreatorFeeManager(address(creatorFeeManager));
 
         console.log("TransferManager address:");
         console.log(transferManagerAddress);
         console.log("LooksRareProtocol address:");
         console.log(looksRareProtocolAddress);
+        console.log("CreatorFeeManagerWithRebates address:");
+        console.log(address(creatorFeeManager));
         console.log("OrderValidatorV2A address:");
         console.log(address(orderValidatorV2A));
         vm.stopBroadcast();

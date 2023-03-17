1f34a705b4621defa50e1b6a173d71b0b3bd36c3
hiroshi <hiroshi@looksrare.org>
2023-03-17 14:45:51 +0000

    chore: Move multiple taker bids tests

--- test/foundry/LooksRareProtocol.executeMultipleTakerBids.t.sol
+++ test/foundry/LooksRareProtocol.executeMultipleTakerBids.t.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.17;
 
 // Libraries
 import {OrderStructs} from "../../contracts/libraries/OrderStructs.sol";
+import {LengthsInvalid} from "../../contracts/errors/SharedErrors.sol";
+import {ERC721TransferFromFail} from "@looksrare/contracts-libs/contracts/lowLevelCallers/LowLevelERC721Transfer.sol";
 
 // Base test
 import {ProtocolBase} from "./ProtocolBase.t.sol";
@@ -28,6 +30,110 @@ contract LooksRareProtocolExecuteMultipleTakerBidsTest is ProtocolBase {
         _setUp();
         vm.prank(_owner);
         mockERC20 = new MockERC20();
+    }
+
+    /**
+     * Three ERC721 are sold through 3 taker bids in one transaction with non-atomicity.
+     */
+    function testThreeTakerBidsERC721() public {
+        uint256 price = 0.015 ether;
+        _setUpUsers();
+
+        uint256 numberOfPurchases = 3;
+
+        BatchExecutionParameters[] memory batchExecutionParameters = _batchERC721ExecutionSetUp(
+            price,
+            numberOfPurchases,
+            QuoteType.Ask
+        );
+
+        // Execute taker bid transaction
+        vm.prank(takerUser);
+        looksRareProtocol.executeMultipleTakerBids{value: price * numberOfPurchases}(
+            batchExecutionParameters,
+            _EMPTY_AFFILIATE,
+            false
+        );
+
+        for (uint256 i; i < numberOfPurchases; i++) {
+            // Taker user has received the asset
+            assertEq(mockERC721.ownerOf(i), takerUser);
+            // Verify the nonce is marked as executed
+            assertEq(looksRareProtocol.userOrderNonce(makerUser, i), MAGIC_VALUE_ORDER_NONCE_EXECUTED);
+        }
+
+        _assertBuyerPaidETH(takerUser, price * numberOfPurchases);
+        _assertSellerReceivedETHAfterStandardProtocolFee(makerUser, price * numberOfPurchases);
+        // No leftover in the balance of the contract
+        assertEq(address(looksRareProtocol).balance, 0);
+    }
+
+    /**
+     * Transaction cannot go through if atomic, goes through if non-atomic (fund returns to buyer).
+     */
+    function testThreeTakerBidsERC721OneFails() public {
+        _setUpUsers();
+
+        uint256 numberOfPurchases = 3;
+        uint256 faultyTokenId = numberOfPurchases - 1;
+
+        BatchExecutionParameters[] memory batchExecutionParameters = _batchERC721ExecutionSetUp(
+            price,
+            numberOfPurchases,
+            QuoteType.Ask
+        );
+
+        // Transfer tokenId = 2 to random user
+        vm.prank(makerUser);
+        mockERC721.transferFrom(makerUser, _randomUser, faultyTokenId);
+
+        /**
+         * 1. The whole purchase fails if execution is atomic
+         */
+        vm.expectRevert(abi.encodeWithSelector(ERC721TransferFromFail.selector));
+        vm.prank(takerUser);
+        looksRareProtocol.executeMultipleTakerBids{value: price * numberOfPurchases}(
+            batchExecutionParameters,
+            _EMPTY_AFFILIATE,
+            true
+        );
+
+        /**
+         * 2. The whole purchase doesn't fail if execution is not-atomic
+         */
+        vm.prank(takerUser);
+        // Execute taker bid transaction
+        looksRareProtocol.executeMultipleTakerBids{value: price * numberOfPurchases}(
+            batchExecutionParameters,
+            _EMPTY_AFFILIATE,
+            false
+        );
+
+        for (uint256 i; i < faultyTokenId; i++) {
+            // Taker user has received the first two assets
+            assertEq(mockERC721.ownerOf(i), takerUser);
+            // Verify the first two nonces are marked as executed
+            assertEq(looksRareProtocol.userOrderNonce(makerUser, i), MAGIC_VALUE_ORDER_NONCE_EXECUTED);
+        }
+
+        // Taker user has not received the asset
+        assertEq(mockERC721.ownerOf(faultyTokenId), _randomUser);
+        // Verify the nonce is NOT marked as executed
+        assertEq(looksRareProtocol.userOrderNonce(makerUser, faultyTokenId), bytes32(0));
+        // Taker bid user pays the whole price
+        assertEq(takerUser.balance, _initialETHBalanceUser - 1 - ((numberOfPurchases - 1) * price));
+        _assertSellerReceivedETHAfterStandardProtocolFee(makerUser, price * (numberOfPurchases - 1));
+        // 1 wei left in the balance of the contract
+        assertEq(address(looksRareProtocol).balance, 1);
+    }
+
+    function testThreeTakerBidsERC721LengthsInvalid() public {
+        _setUpUsers();
+
+        BatchExecutionParameters[] memory batchExecutionParameters = new BatchExecutionParameters[](0);
+        vm.expectRevert(LengthsInvalid.selector);
+        vm.prank(takerUser);
+        looksRareProtocol.executeMultipleTakerBids(batchExecutionParameters, _EMPTY_AFFILIATE, false);
     }
 
     function testCannotTradeIfCurrencyInvalid() public {
--- test/foundry/StandardTransactions.t.sol
+++ test/foundry/StandardTransactions.t.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.17;
 // Libraries, interfaces, errors
 import {OrderStructs} from "../../contracts/libraries/OrderStructs.sol";
 import {LengthsInvalid} from "../../contracts/errors/SharedErrors.sol";
+import {ERC721TransferFromFail} from "@looksrare/contracts-libs/contracts/lowLevelCallers/LowLevelERC721Transfer.sol";
 
 import {CreatorFeeManagerWithRoyalties} from "../../contracts/CreatorFeeManagerWithRoyalties.sol";
 
@@ -18,8 +19,6 @@ import {CollectionType} from "../../contracts/enums/CollectionType.sol";
 import {QuoteType} from "../../contracts/enums/QuoteType.sol";
 
 contract StandardTransactionsTest is ProtocolBase {
-    error ERC721TransferFromFail();
-
     uint256 private constant itemId = 420;
     uint16 private constant NEW_ROYALTY_FEE = uint16(50);
 
@@ -273,111 +272,6 @@ contract StandardTransactionsTest is ProtocolBase {
         looksRareProtocol.executeTakerAsk(takerAsk, makerBid, signature, _EMPTY_MERKLE_TREE, _EMPTY_AFFILIATE);
 
         _assertSuccessfulExecutionThroughWETH(makerUser, takerUser, price, expectedFees);
-    }
-
-    /**
-     * Three ERC721 are sold through 3 taker bids in one transaction with non-atomicity.
-     */
-    function testThreeTakerBidsERC721() public {
-        uint256 price = 0.015 ether;
-        _setUpUsers();
-
-        uint256 numberOfPurchases = 3;
-
-        BatchExecutionParameters[] memory batchExecutionParameters = _batchERC721ExecutionSetUp(
-            price,
-            numberOfPurchases,
-            QuoteType.Ask
-        );
-
-        // Execute taker bid transaction
-        vm.prank(takerUser);
-        looksRareProtocol.executeMultipleTakerBids{value: price * numberOfPurchases}(
-            batchExecutionParameters,
-            _EMPTY_AFFILIATE,
-            false
-        );
-
-        for (uint256 i; i < numberOfPurchases; i++) {
-            // Taker user has received the asset
-            assertEq(mockERC721.ownerOf(i), takerUser);
-            // Verify the nonce is marked as executed
-            assertEq(looksRareProtocol.userOrderNonce(makerUser, i), MAGIC_VALUE_ORDER_NONCE_EXECUTED);
-        }
-
-        _assertBuyerPaidETH(takerUser, price * numberOfPurchases);
-        _assertSellerReceivedETHAfterStandardProtocolFee(makerUser, price * numberOfPurchases);
-        // No leftover in the balance of the contract
-        assertEq(address(looksRareProtocol).balance, 0);
-    }
-
-    /**
-     * Transaction cannot go through if atomic, goes through if non-atomic (fund returns to buyer).
-     */
-    function testThreeTakerBidsERC721OneFails() public {
-        _setUpUsers();
-
-        uint256 price = 1.4 ether;
-        uint256 numberOfPurchases = 3;
-        uint256 faultyTokenId = numberOfPurchases - 1;
-
-        BatchExecutionParameters[] memory batchExecutionParameters = _batchERC721ExecutionSetUp(
-            price,
-            numberOfPurchases,
-            QuoteType.Ask
-        );
-
-        // Transfer tokenId = 2 to random user
-        vm.prank(makerUser);
-        mockERC721.transferFrom(makerUser, _randomUser, faultyTokenId);
-
-        /**
-         * 1. The whole purchase fails if execution is atomic
-         */
-        vm.expectRevert(abi.encodeWithSelector(ERC721TransferFromFail.selector));
-        vm.prank(takerUser);
-        looksRareProtocol.executeMultipleTakerBids{value: price * numberOfPurchases}(
-            batchExecutionParameters,
-            _EMPTY_AFFILIATE,
-            true
-        );
-
-        /**
-         * 2. The whole purchase doesn't fail if execution is not-atomic
-         */
-        vm.prank(takerUser);
-        // Execute taker bid transaction
-        looksRareProtocol.executeMultipleTakerBids{value: price * numberOfPurchases}(
-            batchExecutionParameters,
-            _EMPTY_AFFILIATE,
-            false
-        );
-
-        for (uint256 i; i < faultyTokenId; i++) {
-            // Taker user has received the first two assets
-            assertEq(mockERC721.ownerOf(i), takerUser);
-            // Verify the first two nonces are marked as executed
-            assertEq(looksRareProtocol.userOrderNonce(makerUser, i), MAGIC_VALUE_ORDER_NONCE_EXECUTED);
-        }
-
-        // Taker user has not received the asset
-        assertEq(mockERC721.ownerOf(faultyTokenId), _randomUser);
-        // Verify the nonce is NOT marked as executed
-        assertEq(looksRareProtocol.userOrderNonce(makerUser, faultyTokenId), bytes32(0));
-        // Taker bid user pays the whole price
-        assertEq(takerUser.balance, _initialETHBalanceUser - 1 - ((numberOfPurchases - 1) * price));
-        _assertSellerReceivedETHAfterStandardProtocolFee(makerUser, price * (numberOfPurchases - 1));
-        // 1 wei left in the balance of the contract
-        assertEq(address(looksRareProtocol).balance, 1);
-    }
-
-    function testThreeTakerBidsERC721LengthsInvalid() public {
-        _setUpUsers();
-
-        BatchExecutionParameters[] memory batchExecutionParameters = new BatchExecutionParameters[](0);
-        vm.expectRevert(LengthsInvalid.selector);
-        vm.prank(takerUser);
-        looksRareProtocol.executeMultipleTakerBids(batchExecutionParameters, _EMPTY_AFFILIATE, false);
     }
 
     /**
